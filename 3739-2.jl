8e8dea9b99a4223a197602b8ef91691dbe23ebdc
TZUYU0312 <163967168+TZUYU0312@users.noreply.github.com>
2024-03-20 00:02:52 +0800

    Update 3739-2.jl

--- 3739-2.jl
+++ 3739-2.jl
@@ -1,13 +1,13 @@
 using Statistics
 
-# 计算点之间的距离并分组
+# 計算點之間距離並分組
 function distance_group(x, y, a, b)
     dist = sqrt.((a[:, 1] .- x).^2 .+ (a[:, 2] .- y).^2)
     group = argmin(dist)
     return dist[group], group
 end
 
-# 计算K-mean值
+# 計算K-mean值
 function cal_Kmean(point, a, b)
     d = []
     g = []
@@ -20,19 +20,19 @@ function cal_Kmean(point, a, b)
     return D, d, g
 end
 
-# 点更新
+# 點更新
 function update(group, point, a, b)
     new_a = [mean(point[group .== i, 1]) for i in 1:size(a, 1)]
     new_b = [mean(point[group .== i, 2]) for i in 1:size(b, 1)]
     return new_a, new_b
 end
 
-# 定义15个以上的座标
+# 定義15個以上的座標
 point = [2.0 5.0; 3.0 2.0; 3.0 3.0; 3.0 4.0; 4.0 3.0; 4.0 4.0; 6.0 3.0; 6.0 4.0; 6.0 6.0; 7.0 2.0; 7.0 5.0; 7.0 6.0; 7.0 7.0; 8.0 6.0; 8.0 7.0]
 
-# 定义4个点
+# 定義4個點
 a = [2.0 2.0; 4.0 6.0; 6.0 5.0; 8.0 8.0]
-b = copy(a)  # 初始化 b，使其与 a 具有相同的维度
+b = copy(a)  # 初始化b讓a與b有相同維度
 
 while true
     D_result, _, group = cal_Kmean(point, a, b)
